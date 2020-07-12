71f4ac76205b0264de8147cce9084fbeba747d7c
john-smilga <johnsmilga@gmail.com>
2020-07-12 11:46:56 -0700

    complete starter app

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
   return (
     <div>
       <Dashboard></Dashboard>
+      <Login />
+      <Error />
     </div>
   );
 }
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,7 +3,11 @@ import { Info, Repos, User, Search, Navbar } from '../components';
 import loadingImage from '../images/preloader.gif';
 import { GithubContext } from '../context/context';
 const Dashboard = () => {
-  return <h2>Dashboard Page</h2>;
+  return (
+    <main>
+      <h2>Dashboard Page</h2>;
+    </main>
+  );
 };
 
 export default Dashboard;
